f0f0e444b6faaef015c9c6b5a65b637b856c3aad
Ken Gao <ken_gao@apple.com>
2019-02-15 14:14:09 -0800

    upload code

--- src/test/java/com/apple/json/example/JsonTest.java
+++ src/test/java/com/apple/json/example/JsonTest.java
@@ -44,11 +44,12 @@ public class JsonTest {
         }
         Assert.assertEquals(4, cars.size());
     }
+
     //Test for task 2 lowest price car
     @Test
     public void test2() {
-        float lowest=0;
-        int index=0;
+        float lowest = 0;
+        int index = 0;
 
         JsonParser parser = new JsonParser();
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("rentalcar.json");
@@ -61,16 +62,56 @@ public class JsonTest {
         Assert.assertNotNull(cars);
 
         lowest = cars.get(0).getAsJsonObject().getAsJsonObject("perdayrent").get("Price").getAsFloat();
-        for(int i=1; i<cars.size(); i++){
+        for (int i = 1; i < cars.size(); i++) {
             float price = cars.get(i).getAsJsonObject().getAsJsonObject("perdayrent").get("Price").getAsFloat();
-            float discount= cars.get(i).getAsJsonObject().getAsJsonObject("perdayrent").get("Discount").getAsFloat();
-            price=price-discount;
-            if(price<lowest){
-                 lowest=price;
-                 index=i;
+            float discount = cars.get(i).getAsJsonObject().getAsJsonObject("perdayrent").get("Discount").getAsFloat();
+            price = price - discount;
+            if (price < lowest) {
+                lowest = price;
+                index = i;
             }
         }
         System.out.println(cars.get(index));
+    }
+
+    //Test for task 3, highest revenue generating car, I assume that will be price X rentalcount days -
+    // (year over year maintenance cost + depreciation)
+    @Test
+    public void test3() {
+        float price=0;
+        int rentaldays=0;
+        float yoym=0;
+        float depreciation;
+        float mostProfitable=0;
+        int index=0;
+        float result=0;
+
+        JsonParser parser = new JsonParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("rentalcar.json");
+
+        Reader reader = new InputStreamReader(inputStream);
+        JsonObject jsonObject = parser.parse(reader).getAsJsonObject();
+        Assert.assertNotNull(jsonObject);
+
+        JsonArray cars = jsonObject.getAsJsonArray("Car");
+        Assert.assertNotNull(cars);
+
+        for(int i=0; i<cars.size(); i++ ) {
+            price = cars.get(i).getAsJsonObject().getAsJsonObject("perdayrent").get("Price").getAsFloat();
+            yoym = cars.get(i).getAsJsonObject().getAsJsonObject("metrics").get("yoymaintenancecost").getAsFloat();
+            depreciation= cars.get(i).getAsJsonObject().getAsJsonObject("metrics").get("depreciation").getAsFloat();
+            rentaldays= cars.get(i).getAsJsonObject().getAsJsonObject("metrics").getAsJsonObject("rentalcount").get("yeartodate").getAsInt();
+            //System.out.println(rentaldays);
+            result= price * rentaldays - (yoym+depreciation);
+            //System.out.println(result);
+            if(mostProfitable < result){
+                mostProfitable = result;
+                index = i;
+            }
+        }
+        System.out.println(mostProfitable);
+        System.out.println("most profitable car:"+cars.get(index));
+
     }
 
 }
